a3694a2c049676068aa9aeed6ae9eb132eab2cf6
Absar Ahmad <absar4060@gmail.com>
2022-08-09 23:17:41 +0530

    data decoded from JSON file

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:practice_app/models/catalog.dart';
 import 'package:practice_app/screens/learn_flutter.dart';
 import 'package:practice_app/screens/login.dart';
@@ -6,8 +8,25 @@ import 'package:practice_app/utils/routes.dart';
 import 'package:practice_app/widgets/drawer.dart';
 import 'package:practice_app/widgets/items_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productData = decodedData["items"];
+  }
 
   @override
   Widget build(BuildContext context) {
